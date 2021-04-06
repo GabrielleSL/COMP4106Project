be40191f2fc9e10cf81f20492a08b73031736932
Owner <hersi.abduljabbar@gmail.com>
2021-04-06 16:14:10 -0400

    World Map with tiles Implemented

--- worldMap.py
+++ worldMap.py
@@ -1,15 +1,25 @@
 # list of imported libraries
+import numpy as np
 import random
 import os
 from datetime import datetime
 random.seed(datetime.now())
 
+class Tiles():
+    def __init__(self):
+        self.cross= random.randint(0,4)
+        self.food= random.randint(0,4)
+        self.sight= random.randint(0,4)
+        self.danger= random.randint(0,4)
+        self.getFood= random.randint(0,4)
+        self.stringRep = ' ' 
+
 
 class World():
-    def __init__(self, width, height, default=' '):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.data = [[default for y in range(height)] for x in range(width)]
+        self.data = [[Tiles() for y in range(height)] for x in range(width)]
     
     def set(self, x, y, value):
         self.data[y][x] = value
@@ -17,24 +27,24 @@ class World():
     def get(self, x, y):
         return self.data[y][x]
 
-    def populate(self,num):
-        for x in range(num):
-            self.set(random.randint(0,self.width-1),random.randint(0,self.height-1),'f')
+    # def populate(self,num):
+    #     for x in range(num):
+    #         self.set(random.randint(0,self.width-1),random.randint(0,self.height-1),'f')
     
-    def __str__(self):
-        return self.render()
+    # def __str__(self):
+    #     return self.render()
     
-    def render(self):
-        res = []
+    # def render(self):
+    #     res = []
         
-        horiSplit = ' | '
-        vertSplit = '\n +' + '---+' * self.width + '\n'
+    #     horiSplit = ' | '
+    #     vertSplit = '\n +' + '---+' * self.width + '\n'
         
-        for row in self.data:
-            res.append(horiSplit + horiSplit.join(row) + horiSplit)
+    #     for row in self.data:
+    #         res.append(horiSplit + horiSplit.join(row) + horiSplit)
          
 
-        return vertSplit + vertSplit.join(res) + vertSplit
+    #     return vertSplit + vertSplit.join(res) + vertSplit
 
 def write_world(world, world_map):
     # clears the contents of the text file
@@ -43,7 +53,11 @@ def write_world(world, world_map):
     # open the text file to append data into it
     f = open(world_map, "w")
     # for each node in the explored list, write to the file
-    f.write(f"{world}")
+    for i in range(world.width):
+        for j in range(world.height):
+            f.write(f"{str(world.get(i,j).food)}, ")
+        f.write("\n")
+
     # END FOR
     # close the file
     f.close()
@@ -51,9 +65,7 @@ def write_world(world, world_map):
 
 def main(world_map):
     world = World(10, 10)
-    world.populate(10)
-    world_str = world.render()
-    write_world(world_str,world_map)
+    write_world(world,world_map)
       
 world_map = "test/world_map.txt"
 
