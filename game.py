7301208a9cd55d82774323e7045217cd553dfb9a
Owner <hersi.abduljabbar@gmail.com>
2021-04-13 18:41:43 -0400

    Play

--- game.py
+++ game.py
@@ -16,15 +16,22 @@ def day():
         for org in country.population:
             locx = org[1]
             locy = org[2]
-            newloc = org[0].turn(country.world.get(locx, locy)[0])
-
-            if newloc[0] is not None and newloc[1] is not None:
+            newloc = org[0].turn(country.world.get(locx, locy)[0], locx, locy)
+            
+            if newloc[0] is None and newloc[1] is None:
                 continue
             elif not org[0].status:
                 country.world.set(locx, locy, 1, ' ')
                 continue
-            temp_pop.append([org[0], newloc[0], locy[1]])
+            temp_pop.append([org[0], newloc[0], newloc[1]])
             country.world.set(locx, locy, 1, ' ')
 
+    print(temp_pop)
+    # print(" ")
+    # print(country.genetics(temp_pop))
     new_population = country.populate(country.genetics(temp_pop))
     country.population = new_population
+    # print(new_population)
+
+
+day()
